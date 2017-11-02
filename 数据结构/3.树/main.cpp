dfdb3a1cc8727fcd59fca4ff67054212cf7a0f66
liyunhao <1558198727@qq.com>
2017-11-02 21:24:53 +0800

    树

---
+++ 数据结构/3.树/main.cpp
@@ -0,0 +1,303 @@
+#include <iostream>
+#include"Queue.h"
+using namespace std;
+
+template<class T>
+class BinaryTree;
+
+template<class T>
+class BinaryTreeNode
+{
+   friend class BinaryTree<T>;
+  private:
+      T element; //数据成员
+      BinaryTreeNode<T> *leftChild;
+      BinaryTreeNode<T> *rightChild;
+  public:
+    BinaryTreeNode();
+    BinaryTreeNode(const T &ele);
+    BinaryTreeNode(const T &ele,BinaryTreeNode<T>*l,BinaryTreeNode<T>*r);
+    BinaryTreeNode<T>* getLeftChild()const;
+    BinaryTreeNode<T>* getRightChild()const;
+    void setLeftChild(BinaryTreeNode<T>*l);
+    void setRightChild(BinaryTreeNode<T>*r);
+    void visit();
+    T getValue() const;
+    void setValue(const T & val);
+    bool isLeaf()const;
+};
+
+template<class T>
+BinaryTreeNode<T>::BinaryTreeNode()
+{
+  element=0;
+  leftChild=rightChild=0;
+}
+template<class T>
+BinaryTreeNode<T>::BinaryTreeNode(const T &ele)
+{
+   element=ele;
+   leftChild=rightChild=0;
+}
+template<class T>
+BinaryTreeNode<T>::BinaryTreeNode(const T &ele,BinaryTreeNode<T>*l,BinaryTreeNode<T>*r)
+{
+   element=ele;
+   leftChild=l;
+   rightChild=r;
+};
+template<class T>
+BinaryTreeNode<T>*BinaryTreeNode<T>::getLeftChild()const
+{
+    return this->leftChild;
+}
+
+template<class T>
+BinaryTreeNode<T>* BinaryTreeNode<T>::getRightChild()const
+{
+    return this->rightChild;
+}
+
+
+template<class T>
+void BinaryTreeNode<T>::setLeftChild(BinaryTreeNode<T>*l)
+{
+    this->leftChild=l;
+}
+
+template<class T>
+void BinaryTreeNode<T>::setRightChild(BinaryTreeNode<T>*r)
+{
+    this->rightChild=r;
+}
+template<class T>
+T BinaryTreeNode<T>::getValue() const
+{
+    return this->element;
+}
+
+template<class T>
+void BinaryTreeNode<T>::setValue(const T & val)
+{
+    this->element=val;
+}
+
+template<class T>
+bool BinaryTreeNode<T>::isLeaf()const
+{
+    if(this->leftChild==0 && this->rightChild==0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+template<class T>
+void BinaryTreeNode<T>::visit()
+{
+    cout<<this->element<<" ";
+}
+
+
+template<class T>
+class BinaryTree
+{
+   private:
+       BinaryTreeNode<T> *root;
+   public:
+     BinaryTree();
+     ~BinaryTree();
+
+     bool isEmpty()const;
+     BinaryTreeNode<T> *getRoot()const;
+     BinaryTreeNode<T> * getParent(BinaryTreeNode<T> *current)const;
+     BinaryTreeNode<T> *getLeftSibling(BinaryTreeNode<T> *current)const;//返回current节点的左兄弟
+     BinaryTreeNode<T> *getRightSibling(BinaryTreeNode<T> *current)const;//返回current节点的右兄弟
+     void breadthFirstOrder();//广度优先遍历以root为根节点的子树
+     void preOrderTree();
+     void preOrder(BinaryTreeNode<T> *root);        //先序遍历以root为根节点的子树
+     void inOrderTree();
+     void inOrder(BinaryTreeNode<T> *root);         //中序遍历以root为根节点的子树
+     void postOrderTree();
+     void postOrder(BinaryTreeNode<T> *root);       //后序遍历以root为根节点的子树
+     void levelOrder(BinaryTreeNode<T> *root);      //按层次遍历以root为根节点的子树
+     void deleteBinaryTree(BinaryTreeNode<T> *root);//删除以root为根节点的子树
+     void createTree();
+};
+template<class T>
+BinaryTree<T>::BinaryTree()
+{
+    root=0;
+}
+
+template<class T>
+BinaryTree<T>::~BinaryTree()
+{
+    ;
+}
+
+template<class T>
+bool BinaryTree<T>::isEmpty()const
+{
+    if(root==0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+template<class T>
+BinaryTreeNode<T> *BinaryTree<T>::getRoot()const
+{
+    return root;
+}
+template<class T>
+BinaryTreeNode<T> * BinaryTree<T>::getParent(BinaryTreeNode<T> *current)const//返回current节点的父节点
+{
+    ;
+}
+
+template<class T>
+BinaryTreeNode<T> *BinaryTree<T>::getLeftSibling(BinaryTreeNode<T> *current)const//返回current节点的左兄弟
+{
+    ;
+}
+template<class T>
+BinaryTreeNode<T> *BinaryTree<T>::getRightSibling(BinaryTreeNode<T> *current)const//返回current节点的右兄弟
+{
+
+}
+template<class T>
+void BinaryTree<T>::breadthFirstOrder()//广度优先遍历以root为根节点的子树
+{
+    Queue<BinaryTreeNode<T>* > que;
+    BinaryTreeNode<T> *p=root;
+    if(p)
+    que.EnQueue(p);
+    while(!que.IsEmpty())
+    {
+        que.DeQueue(p);
+        p->visit();
+        if(p->leftChild)
+            que.EnQueue(p->leftChild);
+        if(p->rightChild)
+            que.EnQueue(p->rightChild);
+    }
+    cout<<endl;
+}
+
+//先序遍历以root为根节点的子树,递归实现
+template<class T>
+void BinaryTree<T>::preOrderTree()
+{
+    preOrder(root);
+}
+template<class T>
+void BinaryTree<T>::preOrder(BinaryTreeNode<T> *rt)
+{
+    if(rt)
+    {
+      rt->visit();
+      preOrder(rt->leftChild);
+      preOrder(rt->rightChild);
+    }
+
+}
+//中序遍历以root为根节点的子树
+template<class T>
+void BinaryTree<T>::inOrderTree()
+{
+    inOrder(root);
+}
+template<class T>
+void BinaryTree<T>::inOrder(BinaryTreeNode<T> *rt)
+{
+    if(rt)
+    {
+      inOrder(rt->leftChild);
+      rt->visit();
+      inOrder(rt->rightChild);
+    }
+}
+
+//后序遍历以root为根节点的子树
+template<class T>
+void BinaryTree<T>::postOrderTree()
+{
+    postOrder(root);
+}
+template<class T>
+void BinaryTree<T>::postOrder(BinaryTreeNode<T> *rt)
+{
+    if(rt)
+    {
+      inOrder(rt->leftChild);
+      postOrder(rt->rightChild);
+      rt->visit();
+    }
+}
+template<class T>
+void BinaryTree<T>::levelOrder(BinaryTreeNode<T> *root)      //按层次遍历以root为根节点的子树
+{
+    ;
+}
+template<class T>
+void BinaryTree<T>::deleteBinaryTree(BinaryTreeNode<T> *root)//删除以root为根节点的子树
+{
+    ;
+}
+template<class T>
+void BinaryTree<T>::createTree()
+{
+       BinaryTreeNode<T> *p0,*p1,*p2;
+       p0=new BinaryTreeNode<T>;
+       p1=new BinaryTreeNode<T>;
+       p2=new BinaryTreeNode<T>;
+       root=p0;
+       p0->setValue('A');
+       p1->setValue('B');
+       p0->setLeftChild(p1);
+       p0->setRightChild(0);
+       p0=p0->leftChild;
+       p1=new BinaryTreeNode<T>;
+       p2=new BinaryTreeNode<T>;
+       p1->setValue('C');
+       p2->setValue('D');
+       p0->setLeftChild(p1);
+       p0->setRightChild(p2);
+       p0=p0->rightChild;
+       p1=new BinaryTreeNode<T>;
+       p2=new BinaryTreeNode<T>;
+       p1->setValue('E');
+       p2->setValue('F');
+       p0->setLeftChild(p1);
+       p0->setRightChild(p2);
+       p0=p0->leftChild;
+       p1=new BinaryTreeNode<T>;
+       p2=new BinaryTreeNode<T>;
+       p1=0;
+       p2->setValue('G');
+       p0->setLeftChild(p1);
+       p0->setRightChild(p2);
+}
+
+int main()
+{
+    BinaryTree<char> a;
+    a.createTree();
+    a.breadthFirstOrder();
+    a.preOrderTree();
+    cout<<endl;
+    a.inOrderTree();
+    cout<<endl;
+    a.postOrderTree();
+    cout<<endl;
+    //cout << "Hello world!" << endl;
+    return 0;
+}
